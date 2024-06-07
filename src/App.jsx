3f871c4aaad5952560915f740aeeedc11b010332
dimok <zhasulanov04@list.ru>
2024-06-07 15:52:50 +0500

    Add mantine ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+import {
+  TextInput,
+  PasswordInput,
+  Button,
+  Paper,
+  Container,
+  Title,
+} from "@mantine/core";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MantineProvider>
+      <Login />
+    </MantineProvider>
+  );
 }
 
-export default App
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Container size={420} my={40}>
+      <Title align="center">Welcome back!</Title>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <form>
+          <TextInput
+            label="Email"
+            placeholder="you@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            mt="md"
+          />
+          <Button type="submit" fullWidth mt="xl">
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default App;
